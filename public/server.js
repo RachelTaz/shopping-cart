7d8beb4a2c5b9f6bff94d534b1ff2b563048ce6f
unknown <RachelTaz>
2018-06-09 15:41:39 +0300

    checking the site

--- public/server.js
+++ public/server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 app.listen(8000);
 
-app.use(express.static('public'));
 app.use(express.static('node_modules'));
+app.use(express.static('public'));
